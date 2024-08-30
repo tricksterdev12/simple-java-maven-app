2ea22177aea112161e0ce0e5999cda97d1967a12
tricksterdev12 <midouroseau@gmail.com>
2024-08-30 12:56:26 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Bonjour ! Vous avez réussi le build !";
+    private static final String MESSAGE = "Bonjour ! Vous avez réussi le build v2 !";
 
     public App() {}
 
